4303206195277eb5466025f01df22434e986e4c5
risaco <rhia.cooper@gmail.com>
2017-09-08 22:23:42 -0500

    updated basicCard and clozeCard js files

---
+++ BasicCard.js
@@ -0,0 +1,8 @@
+// *** Constructor for Basic Flashcard ***
+function Basic(front, back) {
+
+	this.front = front;
+	this.back = back;
+}
+
+module.exports = Basic;
---
+++ ClozeCard.js
@@ -0,0 +1,24 @@
+// *** Constructor for Cloze-deleted flashcard ***
+
+
+function Cloze(text, cloze) {
+
+	// make sure all incoming text is lowercase for testing
+	var textLower = text.toLowerCase();
+	var clozeLower = cloze.toLowerCase();
+
+	// test to make sure "cloze" is actually in the "text" -- if not, throw an error
+	if (!textLower.includes(clozeLower)){
+
+		console.log("ERROR: cloze-deletion is not within the full text -- <" + cloze + ">");
+		return;
+	}
+
+
+	this.fullText = text; // full text
+	this.cloze = cloze; // cloze-deleted text only
+	this.partial = text.replace(cloze, "...");
+
+}
+
+module.exports = Cloze;
