d9d79625b6db4acccad94024b690fccca4690f52
risaco <rhia.cooper@gmail.com>
2017-10-23 14:29:10 -0500

    updates to ClozeCard.js

--- ClozeCard.js
+++ ClozeCard.js
@@ -1,19 +1,15 @@
 // *** Constructor for Cloze-deleted flashcard ***
 
-
 function Cloze(text, cloze) {
-
-	
-
 	// Scope-safe constructor code
-	if (!(this instanceof Cloze)) { 
+	if (!(this instanceof Cloze)) {
     	return new Cloze(text, cloze);
   	}
 
 	// make sure all incoming text is lowercase for testing
 	var textLower = text.toLowerCase();
 	var clozeLower = cloze.toLowerCase();
-	
+
 	this.error = (!textLower.includes(clozeLower));
 
 	// test to make sure "cloze" is actually in the "text" -- if not, throw an error
@@ -25,10 +21,6 @@ function Cloze(text, cloze) {
 	this.fullText = text; // full text
 	this.cloze = cloze; // cloze-deleted text only
 	this.partial = text.replace(cloze, "...");
-
-
-	
-
-}
+} // END of Cloze function
 
 module.exports = Cloze;
