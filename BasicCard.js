d94aa5dc965cff9d1cb8519faa90c299e8efa944
risaco <rhia.cooper@gmail.com>
2017-10-23 14:30:20 -0500

    updates to BasicCard.js

--- BasicCard.js
+++ BasicCard.js
@@ -2,12 +2,12 @@
 function Basic(front, back) {
 
 	// Scope-safe constructor code
-	if (!(this instanceof Basic)) { 
+	if (!(this instanceof Basic)) {
     	return new Basic(front, back);
   	}
 
 	this.front = front;
 	this.back = back;
-}
+} // END of Basic function
 
 module.exports = Basic;
